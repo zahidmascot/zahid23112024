43e9b0c3f3803d23b5c7fa28ec368db8f5d02564
zahid1 <zahidmascot@gmail.com>
2024-11-23 01:33:33 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+// const title = document.getElementsByTagName("h1")
+// console.log(title[0].innerText);
+
+// const title = document.getElementsByClassName('Bondhu')
+// // console.log(title[0]);
+
+// for (const tit of title) {
+//     console.log(tit);
+    
+// }
+
+// const title = document.getElementById('lorem')
+// console.log(title.innerText);
+
+// const title = document.querySelectorAll('.Bondhu')
+// console.log(title[0]);
+
+// const change = document.getElementById('change')
+// change.innerText = 'zahidul islam'
+// change.style.color="red"
+// change.style.backgroundColor='blue'
+// change.style.padding='20px'
+// change.style.width='50%'
+// change.style.borderRadius='20px'
+// const container = document.getElementById('container')
+// container.setAttribute('class','ridoy')
+// container.classList.add('shanto')
+// container.classList.remove('shanto')
+
+// console.log(container);
+// console.log(container.innerHTML);
+
+// console.log(container.childNodes[1]);
+
+// const lorem = document.getElementById('lorem')
+// console.log(lorem);
+// lorem.getAttribute('id')
+
+// const lorem = document.getElementById('lorem')
+// lorem.setAttribute('class' , 'ridoy')
+// console.log(lorem);
+// const SetClass=document.getElementsByClassName('ridoy')
+// console.log(SetClass);
+
+// onst lorem = document.getElementById('lorem')
+// lorem.setAttribute('class' , 'ridoy')
+// console.log(lorem);
+// const SetClass=document.getElementsByClassName('ridoy')
+
+// console.log(SetClass);
+
+
+const h2 =document.createElement('h2')
+
+h2.innerText = "amar sonar bangla";
+
+console.log(h2);
+
+const select = document.getElementById('select')
+
+console.log(select);
+select.appendChild(h2)
+
+// const ul = Document.createElement('ul')
+// const li = document.createElement('li')
+// li.innerText = "ami tomay valobasi"
+// console.log(li);
+
